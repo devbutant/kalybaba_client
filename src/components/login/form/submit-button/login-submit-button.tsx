e1b1a56a5a6e0c699858f65ff2f6295f4fe9cdbd
devbu <emmanuelniasse@gmail.com>
2024-09-17 19:27:25 +0200

    rollback

--- src/components/login/form/submit-button/login-submit-button.tsx
+++ src/components/login/form/submit-button/login-submit-button.tsx
@@ -1,34 +1,10 @@
-import axios from "axios";
 import { LoginSubmitButtonProps } from "../../../../types/component-props/login-submit-button";
-import { API } from "../../../../utils/environment";
 import { Button } from "../../../button";
 
 const LoginSubmitButton: React.FC<
     React.PropsWithChildren<LoginSubmitButtonProps>
 > = (props) => {
     const { isSubmitting, errors } = props;
-    const handleTest = async () => {
-        try {
-            const options = {
-                withCredentials: true,
-            };
-
-            const response = await axios.get(`${API.URL}/auth/test`, options);
-
-            // return response.data;
-
-            console.log("Response data:", response.data); // Affichage de la réponse du serveur
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error(
-                    "Axios error:",
-                    error.response?.data || error.message
-                );
-            } else {
-                console.error("Unknown error:", error);
-            }
-        }
-    };
 
     return (
         <>
@@ -38,14 +14,6 @@ const LoginSubmitButton: React.FC<
                 disabled={isSubmitting}
             >
                 {isSubmitting ? "Loading..." : "Connexion"}
-            </Button>
-
-            <Button
-                type="button"
-                className="mt-2 w-full bg-red-600 text-white shadow hover:bg-red-700 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-                onClick={handleTest}
-            >
-                Test
             </Button>
 
             {errors.root && (
