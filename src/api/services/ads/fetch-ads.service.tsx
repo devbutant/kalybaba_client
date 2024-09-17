b35d24c5f556874b2e55f751cc9118eee45b81cb
devbu <emmanuelniasse@gmail.com>
2024-09-17 19:25:28 +0200

    rollback

--- src/api/services/ads/fetch-ads.service.tsx
+++ src/api/services/ads/fetch-ads.service.tsx
@@ -2,8 +2,11 @@ import axios from "axios";
 import { AdDto } from "../../../types";
 import { API } from "../../../utils/environment";
 
-export const fetchAdList = async (): Promise<AdDto[]> => {
+export const fetchAdList = async (token: string | null): Promise<AdDto[]> => {
     const { data: adList } = await axios.get(`${API.URL}/ads`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
         method: "GET",
         withCredentials: true,
     });
