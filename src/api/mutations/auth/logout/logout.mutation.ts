668d66f42f2648261ddff1b316c639bf70fe3542
devbu <emmanuelniasse@gmail.com>
2024-10-21 01:40:35 +0200

    fix: redirect to connexion after logout

--- src/api/mutations/auth/logout/logout.mutation.ts
+++ src/api/mutations/auth/logout/logout.mutation.ts
@@ -7,7 +7,7 @@ export const useLogoutMutation = () => {
 
     return useMutation<void, Error>({
         mutationFn: logoutUser,
-        onSuccess: () => navigate("/"),
+        onSuccess: () => navigate("/connexion"),
         onError: (error) => {
             console.log(error && "toast error");
         },
