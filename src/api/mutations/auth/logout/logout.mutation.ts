5a0a49cbd99b63bd4a88dca8ededdd492c469533
devbu <emmanuelniasse@gmail.com>
2024-10-21 01:43:49 +0200

    fix: redirect to connexion after logout

--- src/api/mutations/auth/logout/logout.mutation.ts
+++ src/api/mutations/auth/logout/logout.mutation.ts
@@ -1,10 +1,7 @@
 import { logoutUser } from "@/api/services/logout";
 import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
 
 export const useLogoutMutation = () => {
-    const navigate = useNavigate();
-
     return useMutation<void, Error>({
         mutationFn: logoutUser,
         onSuccess: () => (window.location.href = "/connexion"),
