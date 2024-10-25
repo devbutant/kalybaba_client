80d6c97385ac837fb5a5eea5963702d9885324c3
devbu <emmanuelniasse@gmail.com>
2024-10-25 21:14:45 +0200

    wip: my ads pagination

--- src/pages/my-ad-list/my-ad-list.tsx
+++ src/pages/my-ad-list/my-ad-list.tsx
@@ -1,13 +1,28 @@
+import { useUserAdListQuery } from "@/api/queries/ads/user-ad-list";
+import { Pagination } from "@/components/pagination";
+import { usePagination } from "@/hooks/pagination";
 import { FC } from "react";
 import { UserAdList } from "../user-ad-list";
 
 const MyAdList: FC = () => {
+    const { currentPage, handlePageChange, perPage } = usePagination();
+    const { data, isLoading, error } = useUserAdListQuery(currentPage, perPage);
+
+    const totalPages = (data?.meta && data?.meta.lastPage) || 1;
+
+    if (isLoading) return <p>Chargement...</p>;
+    if (error) return <p>Erreur lors du chargement des annonces</p>;
+
     return (
         <div className="min-h-screen flex flex-col">
             <main className="flex flex-col flex-1">
-                <h2 className="text-2xl font-semibold mb-6">Mes annonces</h2>
-                <UserAdList />
+                <UserAdList data={data?.data || []} />
             </main>
+            <Pagination
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+                totalPages={totalPages}
+            />
         </div>
     );
 };
