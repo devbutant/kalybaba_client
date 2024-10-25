5e299bc78204ed145cc109ddbec355dc67b6df8e
devbu <emmanuelniasse@gmail.com>
2024-10-25 21:04:30 +0200

    wip: my ads pagination

--- src/api/queries/ads/user-ad-list/user-ad-list-query.ts
+++ src/api/queries/ads/user-ad-list/user-ad-list-query.ts
@@ -1,10 +1,10 @@
 import { fetchUserAdList } from "@/api/services/ads/user-ad-list";
-import { AdDto } from "@/types";
+import { AdResponse } from "@/api/services/ads/user-ad-list/fetch-user-ad-list.service";
 import { useQuery } from "@tanstack/react-query";
 
-export const useUserAdListQuery = () => {
-    return useQuery<AdDto[]>({
-        queryKey: ["ads"],
-        queryFn: fetchUserAdList,
+export const useUserAdListQuery = (page: number = 1, perPage: number = 10) => {
+    return useQuery<AdResponse>({
+        queryKey: ["ads", page, perPage],
+        queryFn: () => fetchUserAdList(page, perPage),
     });
 };
--- src/pages/user-ad-list/user-ad-list.tsx
+++ src/pages/user-ad-list/user-ad-list.tsx
@@ -1,19 +1,33 @@
 import { useUserAdListQuery } from "@/api/queries/ads/user-ad-list";
+import { AdResponse } from "@/api/services/ads/user-ad-list/fetch-user-ad-list.service";
 import { AdCard } from "@/components/ad/ad-card";
+import { usePagination } from "@/hooks/pagination";
 import { FC } from "react";
 
-const UserAdList: FC = () => {
-    const { data: ads } = useUserAdListQuery();
+const UserAdList: FC<AdResponse> = (props) => {
+    const { data: ads } = props;
+    const { currentPage, perPage } = usePagination();
+    const { data, isLoading, error } = useUserAdListQuery(currentPage, perPage);
+
+    const totalCount = (data?.meta && data?.meta.total) || 0;
+
+    if (isLoading) return <p>Chargement...</p>;
+    if (error) return <p>Erreur lors du chargement des annonces</p>;
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
-            {ads &&
-                ads.map((ad, index) => (
-                    <div key={index} className="relative">
-                        <AdCard ad={ad} />
-                    </div>
-                ))}
-        </div>
+        <main className="flex flex-col flex-1">
+            <h2 className="text-2xl font-semibold mb-6">
+                Mes {totalCount} annonces
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+                {ads &&
+                    ads.map((ad, index) => (
+                        <div key={index} className="relative">
+                            <AdCard ad={ad} />
+                        </div>
+                    ))}
+            </div>
+        </main>
     );
 };
 
